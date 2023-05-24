bdd4345f363b21220c9e342af3aa4aae3285465d
v4mp <iqbalmuchlis768@gmail.com>
2023-05-24 11:32:49 +0700

    now i can post request to firestore db

--- Cloud Computing/controllers/userController.js
+++ Cloud Computing/controllers/userController.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const firebase = require('../db');
+const db = require('../db');
 const User = require('../models/user');
-const { getFirestore } = require('firebase/firestore');
+const { getFirestore, collection } = require('firebase/firestore');
+const { doc, setDoc } = require('firebase/firestore');
+const actualDb = getFirestore(db);
+// const dbs = firebase.firestore();
 
 const addUser = async (req, res, next) => {
   try {
     const data = req.body;
-    await getFirestore.collection('users').doc().set(data);
+    // await collection('users').doc('users_data'.toString()).set(data);
+    await setDoc(doc(actualDb, 'users', 'users-data'), data);
     res.send('Record saved successfuly');
   } catch (error) {
     res.status(400).send(error.message);
