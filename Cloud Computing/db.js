dd2bf35fcba1e8a39a2aab606b8de0ec328a1ab1
v4mp <iqbalmuchlis768@gmail.com>
2023-05-29 22:56:16 +0700

    a

--- Cloud Computing/db.js
+++ Cloud Computing/db.js
@@ -1,6 +1,16 @@
 const { initializeApp } = require('firebase/app');
-const config = require('./config');
+// const config = require('./config');
 
-const db = initializeApp(config.firebaseConfig);
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkGQQgqWmd3jDA9KirAM7yjwSWYXLCcUk',
+  authDomain: 'bangkit-product-capstone.firebaseapp.com',
+  projectId: 'bangkit-product-capstone',
+  storageBucket: 'bangkit-product-capstone.appspot.com',
+  messagingSenderId: '847064753196',
+  appId: '1:847064753196:web:e0bacb903c6f3f95209d89',
+  measurementId: 'G-QJ4X5G6Y39',
+};
+
+const db = initializeApp(firebaseConfig);
 
 module.exports = db;
