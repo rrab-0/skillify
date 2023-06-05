f88bac10c38f0264a0df0936d1b246192b4fb9b4
v4mp <iqbalmuchlis768@gmail.com>
2023-06-05 19:13:20 +0700

    added PATCH for users and their jobs

--- Cloud Computing/routes/jobRoutes.js
+++ Cloud Computing/routes/jobRoutes.js
@@ -7,11 +7,13 @@ const {
   getAllJobOfOneUser,
   deleteJob,
   deleteAllJobOfOneUser,
+  updateJob,
 } = require('../controllers/jobController');
 
 router.post('/add-job', addJob);
 router.get('/get-job-id/:id', getJobId);
 router.get('/get-all-jobs-for-a-user', getAllJobOfOneUser);
+router.patch('/update-job/:id', updateJob);
 router.delete('/delete-job/:id', deleteJob);
 router.delete('/delete-all-jobs-for-a-user', deleteAllJobOfOneUser);
 
